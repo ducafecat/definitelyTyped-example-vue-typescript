62e000555c9b3f7c7274452f65368493758c3cc6
TaoZhiWei <tao.hans@gmail.com>
2017-11-29 22:55:50 +0800

    常见定义方式

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
   <div>
-    <input v-model="msg">
-    <p>msg: {{ msg }}</p>
-    <button @click="handelHello">hello</button>
+    <p>请查看 /src/types/** 定义文件例子</p>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Component from 'vue-class-component'
+import Vue from 'vue'
+import Component from 'vue-class-component'
 
-  @Component
-  export default class App extends Vue {
-    // 初始化数据
-    msg: string = 'hello word'
-
-    mounted () {
-    }
-
-    // 方法
-    handelHello () {
-      this.msg = 'haha~~~ hello word!'
-    }
-  }
+@Component
+export default class App extends Vue {
+}
 </script>
